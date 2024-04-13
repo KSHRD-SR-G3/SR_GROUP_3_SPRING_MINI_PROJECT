654fd7b685fc1188b0bc1403cd3715e8d83da3b2
sokmean.kao <sokmean.genz@gmail.com>
2024-04-13 15:28:09 +0700

    work on app-file-controller/profile_image

--- src/main/java/org/example/srg3springminiproject/model/response/APIResponse.java
+++ src/main/java/org/example/srg3springminiproject/model/response/APIResponse.java
@@ -1,4 +1,20 @@
 package org.example.srg3springminiproject.model.response;
 
-public class APIResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class APIResponse<T> {
+    private String message;
+    private T payload;
+    private HttpStatus status;
+    private LocalDateTime dateTime;
 }
