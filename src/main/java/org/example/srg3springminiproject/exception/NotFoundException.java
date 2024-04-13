5bd8471055120bda84656f3d2b9d02f0232ea106
darytasoun <darytasoun@gmail.com>
2024-04-13 14:47:32 +0700

    exception

--- src/main/java/org/example/srg3springminiproject/exception/NotFoundException.java
+++ src/main/java/org/example/srg3springminiproject/exception/NotFoundException.java
@@ -1,4 +1,7 @@
 package org.example.srg3springminiproject.exception;
 
-public class NotFoundException {
+public class NotFoundException extends  RuntimeException{
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
