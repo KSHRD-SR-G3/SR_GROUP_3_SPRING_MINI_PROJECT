e9e619d30ee86a12c195c81057414176414efcf2
BonalihengOun <bonalyhengun@gmail.com>
2024-04-17 18:45:50 +0700

    fix get current user

--- src/main/java/org/example/srg3springminiproject/repository/UserRepository.java
+++ src/main/java/org/example/srg3springminiproject/repository/UserRepository.java
@@ -3,6 +3,7 @@ package org.example.srg3springminiproject.repository;
 import org.apache.ibatis.annotations.*;
 import org.example.srg3springminiproject.model.User;
 import org.example.srg3springminiproject.model.request.ForgetRequest;
+import org.example.srg3springminiproject.model.response.UserResponse;
 
 @Mapper
 public interface UserRepository {
@@ -22,7 +23,10 @@ public interface UserRepository {
     @Select("UPDATE users_tb SET password = #{pass.password} WHERE email = #{email} RETURNING *")
     @ResultMap("authMapper")
     User updatePassword(@Param("pass") ForgetRequest forgetRequest , @Param("email") String email);
+
     @Select("SELECT * FROM users_tb WHERE user_id = #{id}")
-    @ResultMap("authMapper")
-    User getUserById(@Param("userId") Integer id);
+    @Result(property = "userId",column = "user_id")
+    @Result(property = "email", column = "email")
+    @Result(property = "profileImage", column = "profile_image")
+    UserResponse getUserById(@Param("userId") Integer id);
 }
